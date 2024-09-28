3b34e5d70ffe97317533d0c5921cb434a9451403
MarieeMorte <nika5000.com@mail.ru>
2024-09-28 23:43:00 +0300

    refactor: create an auxiliary createWordMap method to reduce code duplication

--- src/main/java/backend/academy/gallows/dictionary/Dictionary.java
+++ src/main/java/backend/academy/gallows/dictionary/Dictionary.java
@@ -10,28 +10,30 @@ public class Dictionary {
     Random random = new Random();
 
     static {
-        Map<Themes, List<String>> easyWords = new HashMap<>();
-        easyWords.put(Themes.FRUITS,
-            List.of("банан", "груша", "дыня", "инжир", "киви", "лайм", "лимон", "манго", "слива", "фрукт", "хурма"));
-        easyWords.put(Themes.BERRIES, List.of("бузина", "калина", "клюква", "малина", "рябина"));
-        easyWords.put(Themes.VEGETABLES,
-            List.of("бобы", "горох", "овощ", "перец", "редис", "тыква", "укроп"));
+        Map<Themes, List<String>> easyWords = createWordMap(Map.of(
+            Themes.FRUITS,
+            List.of("банан", "груша", "дыня", "инжир", "киви", "лайм", "лимон", "манго", "слива", "фрукт", "хурма"),
+            Themes.BERRIES, List.of("бузина", "калина", "клюква", "малина", "рябина"),
+            Themes.VEGETABLES, List.of("бобы", "горох", "овощ", "перец", "редис", "тыква", "укроп")
+        ));
 
-        Map<Themes, List<String>> mediumWords = new HashMap<>();
-        mediumWords.put(Themes.FRUITS, List.of("абрикос", "ананас", "персик", "помело", "яблоко"));
-        mediumWords.put(Themes.BERRIES, List.of("ежевика", "черника"));
-        mediumWords.put(Themes.VEGETABLES,
-            List.of("базилик", "капуста", "морковь", "огурец", "помидор", "свекла", "фасоль", "чеснок", "щавель"));
+        Map<Themes, List<String>> mediumWords = createWordMap(
+            Map.of(Themes.FRUITS, List.of("абрикос", "ананас", "персик", "помело", "яблоко"), Themes.BERRIES,
+                List.of("ежевика", "черника"), Themes.VEGETABLES,
+                List.of("базилик", "капуста", "морковь", "огурец", "помидор", "свекла", "фасоль", "чеснок", "щавель")));
 
-        Map<Themes, List<String>> hardWords = new HashMap<>();
-        hardWords.put(Themes.FRUITS, List.of("бергамот", "виноград", "грейпфрут", "мандарин"));
-        hardWords.put(Themes.BERRIES,
-            List.of("барбарис", "брусника", "голубика", "клубника", "облепиха"));
-        hardWords.put(Themes.VEGETABLES, List.of("баклажаны", "картофель", "петрушка", "сельдерей"));
+        Map<Themes, List<String>> hardWords = createWordMap(
+            Map.of(Themes.FRUITS, List.of("бергамот", "виноград", "грейпфрут", "мандарин"), Themes.BERRIES,
+                List.of("барбарис", "брусника", "голубика", "клубника", "облепиха"), Themes.VEGETABLES,
+                List.of("баклажаны", "картофель", "петрушка", "сельдерей")));
 
         dictionary.put(Difficulties.EASY, easyWords);
         dictionary.put(Difficulties.MEDIUM, mediumWords);
         dictionary.put(Difficulties.HARD, hardWords);
+    }
+
+    private static Map<Themes, List<String>> createWordMap(Map<Themes, List<String>> entries) {
+        return new HashMap<>(entries);
     }
 
     public Difficulties getRandomDifficult() {
